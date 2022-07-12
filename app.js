b5de55117c6f274a22a06372fe613d4737912fa7
stuckinab0x <99902262+stuckinab0x@users.noreply.github.com>
2022-07-12 00:42:48 -0400

    mode names and 4th octave

--- app.js
+++ app.js
@@ -7,27 +7,23 @@ const checkModes = document.getElementById('check-modes');
 const modeDisplay = document.getElementById('mode-display');
 const keyboardImg = document.getElementById('keyboard');
 
-checkSharps.addEventListener('click', () => {
-  if (checkFlats.checked) checkFlats.checked = false;
-  if (checkModes.checked) checkModes.checked = false;
-  checkSharps.checked ? modeDisplay.innerHTML = 'Sharps Only' : modeDisplay.innerHTML = 'All Keys';
-  checkSharps.blur();
+
+
+function handleCheckEvent(checkName) {
+  const checks = [checkSharps, checkFlats, checkModes];
+  checks.splice(checks.indexOf(checkName), 1);
+  checks.forEach(i => i.checked = false);
+  if (!checkName.checked) modeDisplay.innerHTML = 'All Keys';
+  else if (checkName === checkSharps) modeDisplay.innerHTML = 'Sharps Only';
+  else if (checkName === checkFlats) modeDisplay.innerHTML = 'Flats Only';
+  else modeDisplay.innerHTML = 'Modes';
+  checkName.blur();
   resetGame();
-})
-checkFlats.addEventListener('click', () => {
-  if (checkSharps.checked) checkSharps.checked = false;
-  if (checkModes.checked) checkModes.checked = false;
-  checkFlats.checked ? modeDisplay.innerHTML = 'Flats Only' : modeDisplay.innerHTML = 'All Keys';
-  checkFlats.blur();
-  resetGame();
-})
-checkModes.addEventListener('click', () => {
-  if (checkSharps.checked) checkSharps.checked = false;
-  if (checkFlats.checked) checkFlats.checked = false;
-  checkModes.checked ? modeDisplay.innerHTML = 'Modes' : modeDisplay.innerHTML = 'All Keys';
-  checkModes.blur();
-  resetGame();
-})
+}
+
+checkSharps.addEventListener('click', () => handleCheckEvent(checkSharps));
+checkFlats.addEventListener('click', () => handleCheckEvent(checkFlats));
+checkModes.addEventListener('click', () => handleCheckEvent(checkModes));
 
 document.addEventListener('keyup', e => {
   if (e.code === 'Space') generateVisible();
@@ -38,73 +34,73 @@ const modeScales = [
     name: 'C Major',
     notes: ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C2', 'B2'],
     special: [],
-    modeDefinition: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+    modeDefinition: ['C Ionian (Major)', 'D Dorian', 'E Phrygian', 'F Lydian', 'G Mixolydian', 'A Aeolian (Minor)', 'B Locrian'],
   },
   {
     name: 'Db Major',
     notes: ['C1', 'Csharp1', 'Dsharp1', 'F1', 'Fsharp1', 'Gsharp1', 'Asharp1', 'C2', 'Csharp2', 'Dsharp2', 'F2', 'Fsharp2', 'Gsharp2', 'Asharp2'],
     special: ['flat'],
-    modeDefinition: ['C', 'Db', 'Eb', 'F', 'F#/Gb', 'Ab', 'Bb'],
+    modeDefinition: ['C Locrian', 'Db Ionian (Major)', 'Eb Dorian', 'F Phrygian', 'Gb Lydian', 'Ab Mixolydian', 'Bb Aeolian (Minor)'],
   },
   {
     name: 'D Major',
     notes: ['Csharp1', 'D1', 'E1', 'Fsharp1', 'G1', 'A1', 'B1', 'Csharp2', 'D2', 'E2', 'Fsharp2', 'G2', 'A2', 'B2'],
     special: ['sharp'],
-    modeDefinition: ['Db', 'D', 'E', 'F#/Gb', 'G', 'A', 'B'],
+    modeDefinition: ['C# Locrian', 'D Ionian (Major)', 'E Dorian', 'F# Phrygian', 'G Lydian', 'A Mixolydian', 'B Aeolian (Minor)'],
   },
   {
     name: 'Eb Major',
     notes: ['C1', 'D1', 'Dsharp1', 'F1', 'G1', 'Gsharp1', 'Asharp1', 'C2', 'D2', 'Dsharp2', 'F2', 'G2', 'Gsharp2', 'Asharp2'],
     special: ['flat'],
-    modeDefinition: ['C', 'E', 'Eb', 'F', 'G', 'Ab', 'Bb'],
+    modeDefinition: ['C Aeolian (Minor)', 'D Locrian', 'Eb Ionian (Major)', 'F Dorian', 'G Phrygian', 'Ab Lydian', 'Bb Mixolydian'],
   },
   {
     name: 'E Major',
     notes: ['Csharp1', 'Dsharp1', 'E1', 'Fsharp1', 'Gsharp1', 'A1', 'B1', 'Csharp2', 'Dsharp2', 'E2', 'Fsharp2', 'Gsharp2', 'A2', 'B2'],
     special: ['sharp'],
-    modeDefinition: ['Db', 'Eb', 'E', 'F#/Gb', 'Ab', 'A', 'B'],
+    modeDefinition: ['C# Aeolian (Minor)', 'D# Locrian', 'E Ionian (Major)', 'F# Dorian', 'G# Phrygian', 'A Lydian', 'B Mixolydian'],
   },
   {
     name: 'F Major',
     notes: ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'Asharp1', 'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'Asharp2'],
     special: ['flat'],
-    modeDefinition: ['C', 'D', 'E', 'F', 'G', 'A', 'Bb'],
+    modeDefinition: ['C Mixolydian', 'D Aeolian (Minor)', 'E Locrian', 'F Ionian (Major)', 'G Dorian', 'A Phrygian', 'Bb Lydian'],
   },
   {
     name: 'F# Major/Gb Major',
     notes: ['Csharp1', 'Dsharp1', 'F1', 'Fsharp1', 'Gsharp1', 'Asharp1', 'B1', 'Csharp2', 'Dsharp2', 'F2', 'Fsharp2', 'Gsharp2', 'Asharp2', 'B2'],
     special: ['sharp', 'flat'],
-    modeDefinition: ['Db', 'Eb', 'F', 'F#/Gb', 'Ab', 'Bb', 'B'],
+    modeDefinition: ['C#/Db Mixolydian', 'Eb Aeolian (Minor)', 'F Locrian', 'F#/Gb Ionian (Major)', 'G#/Ab Dorian', 'A#/Bb Phrygian', 'B Lydian'],
   },
   {
     name: 'G Major',
     notes: ['C1', 'D1', 'E1', 'Fsharp1', 'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'Fsharp2', 'G2', 'A2', 'B2'],
     special: ['sharp'],
-    modeDefinition: ['C', 'D', 'E', 'F#/Gb', 'G', 'A', 'B'],
+    modeDefinition: ['C Lydian', 'D Mixolydian', 'E Aeolian (Minor)', 'F# Locrian', 'G Ionian (Major)', 'A Dorian', 'B Phrygian'],
   },
   {
     name: 'Ab Major',
     notes: ['C1', 'Csharp1', 'Dsharp1', 'F1', 'G1', 'Gsharp1', 'Asharp1', 'C2', 'Csharp2', 'Dsharp2', 'F2', 'G2', 'Gsharp2', 'Asharp2'],
     special: ['flat'],
-    modeDefinition: ['C', 'Db', 'Eb', 'F', 'G', 'Ab', 'Bb'],
+    modeDefinition: ['C Phrygian', 'Db Lydian', 'Eb Mixolydian', 'F Aeolian (Minor)', 'G Locrian', 'Ab Ionian (Major)', 'Bb Dorian'],
   },
   {
     name: 'A Major',
     notes: ['Csharp1', 'D1', 'E1', 'Fsharp1', 'Gsharp1', 'A1', 'B1', 'Csharp2', 'D2', 'E2', 'Fsharp2', 'Gsharp2', 'A2', 'B2'],
     special: ['sharp'],
-    modeDefinition: ['Db', 'D', 'E', 'F#/Gb', 'Ab', 'A', 'B'],
+    modeDefinition: ['C# Phrygian', 'D Lydian', 'E Mixolydian', 'F# Aeolian (Minor)', 'G# Locrian', 'A Ionian (Major)', 'B Dorian'],
   },
   {
     name: 'Bb Major',
     notes: ['C1', 'D1', 'Dsharp1', 'F1', 'G1', 'A1', 'Asharp1', 'C2', 'D2', 'Dsharp2', 'F2', 'G2', 'A2', 'Asharp2'],
     special: ['flat'],
-    modeDefinition: ['C', 'D', 'Eb', 'F', 'G', 'A', 'Bb'],
+    modeDefinition: ['C Dorian', 'D Phrygian', 'Eb Lydian', 'F Mixolydian', 'G Aeolian (Minor)', 'A Locrian', 'Bb Ionian (Major)'],
   },
   {
     name: 'B Major',
     notes: ['Csharp1', 'Dsharp1', 'E1', 'Fsharp1', 'Gsharp1', 'Asharp1', 'B1', 'Csharp2', 'Dsharp2', 'E2', 'Fsharp2', 'Gsharp2', 'Asharp2', 'B2'],
     special: ['sharp'],
-    modeDefinition: ['Db', 'Eb', 'E', 'F#/Gb', 'Ab', 'Bb', 'B'],
+    modeDefinition: ['C# Dorian', 'D# Phrygian', 'E Lydian', 'F# Mixolydian', 'G# Aeolian (Minor)', 'A# Locrian', 'B Ionian (Major)'],
   }
 ]
 
@@ -174,20 +170,18 @@ const fullScales = [
 let guessing = false; 
 let currentKey;
 let currentStart;
-let scaleIndex;
-let startingKey;
 
 const sharpsOnly = fullScales.filter(i => i.special.includes('sharp') );
 const flatsOnly = fullScales.filter(i => i.special.includes('flat') );
 
 function generateVisible() {
   if (guessing && !checkModes.checked) {
-    keyDisplay.innerHTML = currentKey;
+    keyDisplay.innerHTML = currentKey.name;
     guessing = false;
     goButton.innerHTML = 'Next Scale';
     return;
   } else if (guessing) {
-    // keyDisplay.innerHTML = startingKey + modeNames[currentStart];
+    keyDisplay.innerHTML = currentKey.modeDefinition[currentStart];
     goButton.innerHTML = 'Next Scale';
     guessing = false;
     return;
@@ -196,25 +190,23 @@ function generateVisible() {
   allKeys.forEach(i => i.classList.remove('key-show'));
 
   let chosenKey;
-  let chosenStart
-  const validKeys = checkModes.checked ? getValidKeys(true) : getValidKeys();
 
   if (!checkModes.checked) {
+    const validKeys = getValidKeys();
     do {
       chosenKey = validKeys[Math.floor(Math.random() * validKeys.length)];
-    } while (currentKey === chosenKey.name);
+    } while (currentKey === chosenKey);
     chosenKey.notes.forEach(i => {
       const keysOfType = Array.from(document.getElementsByClassName(i));
       keysOfType.forEach(i => i.classList.add('key-show'))
     })
     keyboardImg.classList.add('keyboard-hide');  
   } else {
-    scaleIndex = [Math.floor(Math.random() * 12)];
-    chosenKey = validKeys[scaleIndex];
+    chosenKey = modeScales[Math.floor(Math.random() * 12)];
+    let chosenStart;
     do chosenStart = Math.floor(Math.random() * 7);
     while (chosenStart === currentStart);
     currentStart = chosenStart;
-    startingKey = chosenKey.modeDefinition[chosenStart];
     const visible = chosenKey.notes.slice(chosenStart, chosenStart + 7);
     visible.forEach(i => {
       document.getElementById(i).classList.add('key-show');
@@ -223,16 +215,15 @@ function generateVisible() {
 
   keyDisplay.innerHTML = '????';
   guessing = true;
-  currentKey = chosenKey.name;
+  currentKey = chosenKey;
   goButton.innerHTML = 'Show Answer';
 }
 
-function getValidKeys(modes = false) {
+function getValidKeys() {
   let validKeys;
-  if (!modes && checkSharps.checked) validKeys = sharpsOnly;
-  else if (!modes && checkFlats.checked) validKeys = flatsOnly;
-  else if (!modes) validKeys = fullScales;
-  else validKeys = modeScales;
+  if (checkSharps.checked) validKeys = sharpsOnly;
+  else if (checkFlats.checked) validKeys = flatsOnly;
+  else validKeys = fullScales;
   return validKeys;
 }
 
@@ -243,6 +234,3 @@ function resetGame() {
   goButton.innerHTML = 'Next Scale';
   keyboardImg.classList.remove('keyboard-hide');
 }
-
-const modeNames = ['Ionian (Major)', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian (Minor)', 'Locrian']
-const plainKeyNames = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'F#/Gb', 'G', 'Ab', 'A', 'Bb', 'B']
